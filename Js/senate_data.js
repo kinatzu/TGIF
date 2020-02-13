0dc47fecfa92eb227584cfea20a82f425108be3d
[kenneth] <[k.exposito.ubiqum@gmail.com]>
2020-02-13 13:19:33 +0100

    Minor changes

--- Js/senate_data.js
+++ Js/senate_data.js
@@ -1,3 +1,25 @@
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//FETCH
+
 var members
 
 fetch("https://api.propublica.org/congress/v1/113/senate/members.json", {
